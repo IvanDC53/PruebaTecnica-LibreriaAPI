d25b1b183b556e0a2a3401666ffe92130d1293fb
Ivan Del Castillo <ivan03557@gmail.com>
2026-07-16 08:35:08 -0400

    fix: corrige mapeo de llaves foraneas en modelos Libro y Prestamo

--- Models/Libro.cs
+++ Models/Libro.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
 namespace LibreriaAPI.Models
@@ -7,14 +8,18 @@ namespace LibreriaAPI.Models
     {
         [Key]
         public int libro_id { get; set; }
-        public string titulo { get; set; } = string.Empty;
-        public int autor_id { get; set; }
-        public int año_publicacion { get; set; }
-        public string genero { get; set; } = string.Empty;
 
+        public string titulo { get; set; } = string.Empty;
+
+        public int año_publicacion { get; set; }
+
+        public int autor_id { get; set; }
+
+        [ForeignKey("autor_id")]
         [JsonIgnore]
         public Autor? Autor { get; set; }
+
         [JsonIgnore]
-        public List<Prestamo> Prestamos { get; set; } = new List<Prestamo>();
+        public ICollection<Prestamo> Prestamos { get; set; } = new List<Prestamo>();
     }
 }
--- Models/Prestamo.cs
+++ Models/Prestamo.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
 namespace LibreriaAPI.Models
@@ -7,10 +8,14 @@ namespace LibreriaAPI.Models
     {
         [Key]
         public int prestamo_id { get; set; }
+
         public int libro_id { get; set; }
+
         public DateTime fecha_prestamo { get; set; }
+
         public DateTime? fecha_devolucion { get; set; }
 
+        [ForeignKey("libro_id")]
         [JsonIgnore]
         public Libro? Libro { get; set; }
     }
